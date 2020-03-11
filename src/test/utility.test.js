d9989f1d68469285ad0dbcf8a7229efac4c39c32
Philip Saa <cowglow@gmail.com>
2020-03-11 11:49:26 +0100

    Utility test initial commit
    Signed-off-by: Philip Saa <cowglow@gmail.com>

--- src/test/utility.test.js
+++ src/test/utility.test.js
@@ -4,8 +4,8 @@ test("Interval Timestamp Tester", () => {
   const tweets = ["test", "one", "two", "three"];
   const data = {
     selector: "S-E",
-    start_time: "2020-03-02T17:19:39",
-    end_time: "2020-03-02T17:33:39",
+    start_time: "2020-03-02T17:00:00",
+    end_time: "2020-03-02T17:40:00",
     interval_value: null
   };
 
@@ -18,21 +18,30 @@ test("Interval Timestamp Tester", () => {
   // Interval duration in minutes.
   let interval = Math.floor(data.interval_value * 60000);
 
+  let diff = null;
   // Updates the interval based on the range from start to finish.
   if (data.selector === "S-E") {
-    const diff = Math.abs(endTime - startTime) / tweets.length;
+    diff = Math.abs(endTime - startTime) / tweets.length;
     interval = Math.floor(diff / 60000);
   }
 
-  console.log("================================================");
-  console.log({
-    // startTime: startTime,
-    // endTime: endTime,
-    interval: interval
-  });
+  const collector = [];
   for (let i = 0; i < tweets.length; ++i) {
-    const timestamp = intervalTimeStamp(startTime, interval, i + 1);
-    console.log(timestamp);
+    const timestamp = intervalTimeStamp(startTime, interval * 60 * 1000, i + 1);
+    collector.push(
+      "\n" +
+        ("0" + timestamp.getHours()).slice(-2) +
+        ":" +
+        ("0" + timestamp.getMinutes()).slice(-2) +
+        ":00 PM"
+    );
   }
-  console.log("================================================");
+
+  console.log("tweet count:", tweets.length);
+  console.log(
+    "FROM\t( " + startTime.toLocaleTimeString(),
+    ")\nTO\t\t( " + endTime.toLocaleTimeString() + " )"
+  );
+  console.log("interval time:", interval + " mins \t" + diff + " ms");
+  console.log("timestamps:", collector.toString());
 });
